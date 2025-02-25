727179c1c8a4f5b1adb32c0071fdaffa68ef6b93
Clemence PAVY <clemence.pavy@etu.unicaen.fr>
2025-02-25 15:01:50 +0100

    td2 - v-for + computed

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 
 <script setup>
 
+  //WYSIWYG => content
+
   //1. Rendu Déclaratif
   let text0 = "Rendu Déclaratif";
+
+  //avant les directives
+
 
   /*2. directives
     ☻ v-bind => :
@@ -16,23 +21,45 @@
       ☺ binding bilatéral
       ☺ v-model="text2"
 
-    ☺ v-for 
+    ☻ v-for 
+
+    ☻ v-if
+      ☺ peut caché automatiquement
+
+    ☺ ref : update la valeur automatiquement
   */
   
-  import {ref} from "vue";
+  import {ref, computed} from "vue";
 
-  //let text = "Directives"; --on commente pour utiliser une constante
-  const text = ref("Directives");
-
-  /*
+  /*V1
+  let text = "Directives"; --on commente pour utiliser une constante
+  
   function updateText(event) {
     text.value = event.target.value;
   }*/
+  const text = ref("");
+
+  const trimmedText = computed(() => text.value.trim());
 
   const posts = ref([]);
 
   function addPost() {
-    posts.value.push(text.value);
+    const newPost = {
+      id: Math.random.toString(36).substring(2),
+      content: trimmedText.value,
+      createdAt: new Date(),
+      autor: {
+        username: "Clem",
+        avatarUrl: "https://media1.tenor.com/m/a5RGfluwSOgAAAAd/sylvian-delhoumi.gif"
+      }
+    };
+
+    //https://i.pinimg.com/originals/f3/13/e1/f313e1532486c2f96301ee5c5b14037d.gif raclette
+    //https://media1.tenor.com/m/a5RGfluwSOgAAAAd/sylvian-delhoumi.gif delhoumi
+    
+    //V1 posts.value.push(trimmedText.value);
+    posts.value.push(newPost);
+    
     text.value = "";
     console.log(posts.value);
   }
@@ -44,10 +71,22 @@
     <div class="container">
         <form class="card" @submit.prevent="addPost">
           <textarea name="post" id="post" placeholder="Quoi de neuf ?" v-model="text" ></textarea>
-          <button type="sumbit">Publier</button>
+          <button type="sumbit" :disabled="!trimmedText">Publier</button>
         </form>
+
+        <h2 v-if="!posts.length">Aucun post pour le moment. (il supp quand c'est faux)</h2>
+        <h2 v-show="!posts.length">Pour voir quand on fait "inspect "(genre réaparaitre souvent)</h2>
         
-        <p v-for="(post, index) in posts" :keys="index">{{ post }}</p>
+        <article v-for="(post, index) in orderBy(posts, Date)" :keys="index" class="card">
+
+          <header>
+            <img :src="post.autor.avatarUrl" alt="avatar" width="36" height="36">
+            <a>{{ post.autor.username }}</a>
+          </header>
+          
+          <p>{{ post.content }}</p>
+
+        </article>
 
         <!--<p>{{ text }}</p>-->
         <!--<p>{{ text0 }}</p>-->
@@ -97,4 +136,30 @@ button {
   height: 40px;
   padding: 0 1rem;
 }
+
+button:disabled {
+  opacity: 0.4;
+  cursor: not-allowed;
+}
+
+article {
+  padding: 1rem;
+  overflow: hidden;
+}
+
+article p /*article > p (1er article*/ {
+  white-space: pre-wrap;
+}
+
+article header {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  margin-bottom: 1rem;
+}
+
+article img {
+  border-radius: 50%;
+}
+
 </style>
