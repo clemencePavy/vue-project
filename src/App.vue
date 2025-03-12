ba3b0d5341fd06ccda64836893360fa9fabf951a
Clemence PAVY <clemence.pavy@etu.unicaen.fr>
2025-03-12 09:09:49 +0100

    tp3

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,38 @@
 
 <script setup>
-
-  //WYSIWYG => content
-
-  //1. Rendu Déclaratif
-  let text0 = "Rendu Déclaratif";
-
-  //avant les directives
-
-
-  /*2. directives
-    ☻ v-bind => :
-      ☺ v-bind:value="text" -> :value="text"
-
-    ☻ v-on => @ 
-      ☺ écoutes les balise sur les évènements (ex : input)
-      ☺ v-on:input="updateText" -> @input="updateText"
-
-    ☻ v-model =>
-      ☺ binding bilatéral
-      ☺ v-model="text2"
-
-    ☻ v-for 
-
-    ☻ v-if
-      ☺ peut caché automatiquement
-
-    ☺ ref : update la valeur automatiquement
-  */
   
   import {ref, computed} from "vue";
+  import PostCard from "@/components/PostCard.vue";
 
-  /*V1
-  let text = "Directives"; --on commente pour utiliser une constante
-  
-  function updateText(event) {
-    text.value = event.target.value;
-  }*/
+
   const text = ref("");
 
   const trimmedText = computed(() => text.value.trim());
 
   const posts = ref([]);
+  //const sortedPosts = computed( () => [...posts.value].sort( (a, b) => b.createdAt - a.createdAt ));
+  const sortedPosts = computed( () => posts.value.toSorted( (a, b) => b.createdAt - a.createdAt ));
 
   function addPost() {
     const newPost = {
-      id: Math.random.toString(36).substring(2),
+      id: Math.random().toString(36).substring(2),
       content: trimmedText.value,
       createdAt: new Date(),
+      likes: 0,
       autor: {
         username: "Clem",
         avatarUrl: "https://media1.tenor.com/m/a5RGfluwSOgAAAAd/sylvian-delhoumi.gif"
       }
     };
 
-    //https://i.pinimg.com/originals/f3/13/e1/f313e1532486c2f96301ee5c5b14037d.gif raclette
-    //https://media1.tenor.com/m/a5RGfluwSOgAAAAd/sylvian-delhoumi.gif delhoumi
-    
-    //V1 posts.value.push(trimmedText.value);
     posts.value.push(newPost);
     
     text.value = "";
     console.log(posts.value);
+  }
+
+  function deletePost(id) {
+    posts.value = posts.value.filter( (post) => post.id !== id );
   }
 
 </script>
@@ -69,97 +40,16 @@
 <template>
   <main>
     <div class="container">
+
         <form class="card" @submit.prevent="addPost">
           <textarea name="post" id="post" placeholder="Quoi de neuf ?" v-model="text" ></textarea>
           <button type="sumbit" :disabled="!trimmedText">Publier</button>
         </form>
 
-        <h2 v-if="!posts.length">Aucun post pour le moment. (il supp quand c'est faux)</h2>
-        <h2 v-show="!posts.length">Pour voir quand on fait "inspect "(genre réaparaitre souvent)</h2>
-        
-        <article v-for="(post, index) in orderBy(posts, Date)" :keys="index" class="card">
+        <h2 v-if="!posts.length">Aucun post pour le moment.</h2>
 
-          <header>
-            <img :src="post.autor.avatarUrl" alt="avatar" width="36" height="36">
-            <a>{{ post.autor.username }}</a>
-          </header>
-          
-          <p>{{ post.content }}</p>
+        <PostCard v-for="(post, index) in sortedPosts" :key="index" class="card" :post="post" @delete="deletePost"/>
 
-        </article>
-
-        <!--<p>{{ text }}</p>-->
-        <!--<p>{{ text0 }}</p>-->
     </div>
   </main>
 </template>
-
-<style scoped>
-  .container {
-  height: 100vh;
-  margin: 0 auto;
-  max-width: 640px;
-}
-.card {
-  background-color: var(--color-bg-secondary);
-  border-radius: 10px;
-  border: 1px solid var(--color-border);
-  margin-bottom: 1rem;
-}
-
-form {
-  display: flex;
-  flex-direction: column;
-  margin-top: 1rem;
-  padding: 1rem 1.5rem;
-  width: 100%;
-}
-textarea {
-  background: none;
-  border: none;
-  color: var(--color-text-primary);
-  flex: 1;
-  margin-bottom: 1rem;
-  outline: none;
-  padding: 0.5rem 0;
-  resize: none;
-  field-sizing: content;
-}
-button {
-  align-self: flex-end;
-  background: none;
-  border-radius: 10px;
-  border: 1px solid var(--color-border);
-  color: var(--color-text-primary);
-  cursor: pointer;
-  font-size: 1rem;
-  height: 40px;
-  padding: 0 1rem;
-}
-
-button:disabled {
-  opacity: 0.4;
-  cursor: not-allowed;
-}
-
-article {
-  padding: 1rem;
-  overflow: hidden;
-}
-
-article p /*article > p (1er article*/ {
-  white-space: pre-wrap;
-}
-
-article header {
-  display: flex;
-  align-items: center;
-  gap: 0.5rem;
-  margin-bottom: 1rem;
-}
-
-article img {
-  border-radius: 50%;
-}
-
-</style>
---
+++ src/components/PostCard.vue
@@ -0,0 +1,56 @@
+<script setup>
+
+    
+    import { TrashIcon, HeartIcon } from "@heroicons/vue/24/outline";
+    import { HeartIcon as HeartIconSolid } from "@heroicons/vue/24/solid";
+
+    const props = defineProps({
+        post: {
+           type: Object,
+           required: true, 
+        },
+    });
+
+    const emit = defineEmits(["delete", "like"]);
+
+    const isLiked = false;
+
+    function deletePost() {
+        emit("delete", props.post.id);
+    }
+
+    function likePost() {
+        if(isLiked) {
+            isLiked = false;
+            emit("like", false);
+        }
+        else {
+            isLiked = true;
+            emit("like", true);
+        }
+    }
+
+</script>
+
+<template>
+    <article class="card">
+
+        <header>
+            <img :src="post.autor.avatarUrl" alt="avatar" width="36" height="36" class="avatar">
+            <a>{{ post.autor.username }}</a>
+        </header>
+
+        <p>{{ post.content }}</p>
+
+        <footer>
+            <button @click="likePost">
+                <HeartIcon @click=""/>
+                <HeartIconSolid @click=""/>
+            </button>
+            <button @click="deletePost" class="btn-icon">
+                <TrashIcon/>
+            </button>
+        </footer>
+
+    </article>
+</template>
