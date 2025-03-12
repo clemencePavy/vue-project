b9f236ac0955e02efccb769014a5fde3262eaca2
Clemence PAVY <clemence.pavy@etu.unicaen.fr>
2025-03-12 10:53:33 +0100

    tp4 - api + restructuration router + page user

--- src/components/PostCard.vue
+++ src/components/PostCard.vue
@@ -1,8 +1,10 @@
 <script setup>
 
-    
     import { TrashIcon, HeartIcon } from "@heroicons/vue/24/outline";
     import { HeartIcon as HeartIconSolid } from "@heroicons/vue/24/solid";
+    import { useRouter } from "vue-router";
+
+    const router = useRouter();
 
     const props = defineProps({
         post: {
@@ -13,22 +15,16 @@
 
     const emit = defineEmits(["delete", "like"]);
 
-    const isLiked = false;
-
     function deletePost() {
         emit("delete", props.post.id);
     }
 
     function likePost() {
-       /* if(isLiked) {
-            isLiked = false;
-            emit("like", false);
-        }
-        else {
-            isLiked = true;
-            emit("like", true);
-        }*/
         emit("like", props.post.id);
+    }
+
+    function gotToUserProfile() {
+        router.push({ name: "user", params: {username: props.post.author.username} });
     }
 
 </script>
@@ -37,16 +33,18 @@
     <article class="card">
 
         <header>
-            <img :src="post.autor.avatarUrl" alt="avatar" width="36" height="36" class="avatar">
-            <a>{{ post.autor.username }}</a>
+            <img :src="post.author.avatarUrl" alt="avatar" width="36" height="36" class="avatar">
+            <a @click="gotToUserProfile">{{ post.author.username }}</a>
         </header>
 
         <p>{{ post.content }}</p>
 
         <footer>
-            <button @click="likePost">
-                <HeartIcon @click=""/>
-                <HeartIconSolid @click=""/>
+            <button @click="likePost" class="btn-icon">
+                <!--<HeartIcon v-if="!post.liked"/>
+                <HeartIconSolid v-else class="active"/>-->
+
+                <component :is="post.liked ? HeartIconSolid : HeartIcon" :class="{active: post.liked}"/>
             </button>
             <button @click="deletePost" class="btn-icon">
                 <TrashIcon/>
